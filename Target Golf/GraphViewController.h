c4d9557883e073d71aa92839eb2513ab867aee25
bertrandbawbag <finbar_wright@hotmail.com>
2013-07-02 20:49:32 +0100

    Added view and controller graph view Changed button from a view to a button for now

---
+++ Target Golf/GraphViewController.h
@@ -0,0 +1,13 @@
+//
+//  GraphViewController.h
+//  Target Golf
+//
+//  Created by Claire Wright on 02/07/2013.
+//  Copyright (c) 2013 Claire Wright. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GraphViewController : UIViewController
+
+@end
